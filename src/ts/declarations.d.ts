7ff0438aea896c3fd34c7098b4ce707c7f433ef5
Romain Casati <romain.casati@ac-orleans-tours.fr>
2021-11-22 15:55:18 +0100

    Using one big declaration file.

---
+++ src/ts/declarations.d.ts
@@ -0,0 +1,7 @@
+declare module "jquery";
+declare module "jquery-ui";
+declare module "es6-promise/dist/promise-1.0.0";
+declare module "react/cjs/react.production.min";
+declare module "react-dom/cjs/react-dom.production.min";
+declare module "create-react-class/create-react-class.min";
+declare module "requirejs/require";
